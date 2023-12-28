15fdcf8975a4895bf195d4d8842b0d5be192b7b8
burntsaxphonereed <151691868+burntsaxphonereed@users.noreply.github.com>
2023-12-27 19:14:36 -0500

    Add files via upload

---
+++ script_component.hpp
@@ -0,0 +1,26 @@
+#define COMPONENT itanoa
+#define COMPONENT_BEAUTIFIED itanoa
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_ITANOA
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_ITANOA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_ITANOA
+#endif
+#include "\x\\addons\main\script_macros.hpp"
+#define COMPONENT itanoa
+#define COMPONENT_BEAUTIFIED itanoa
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_ITANOA
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_ITANOA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_ITANOA
+#endif
+#include "\x\\addons\main\script_macros.hpp"
